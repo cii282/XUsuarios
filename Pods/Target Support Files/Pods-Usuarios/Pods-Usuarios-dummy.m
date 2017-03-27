31ef58cf6191d860d860b78dbaf107ef6bb9d65a
Patricia Carvalho <patricia.oliveira@wiinc.com.br>
2017-03-27 14:28:14 -0300

    Criação do projeto com cocoapods

---
+++ Pods/Target Support Files/Pods-Usuarios/Pods-Usuarios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Usuarios : NSObject
+@end
+@implementation PodsDummy_Pods_Usuarios
+@end
---
+++ Usuarios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Usuarios
+//
+//  Created by Patricia Carvalho on 27/03/17.
+//  Copyright © 2017 Patricia Carvalho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
